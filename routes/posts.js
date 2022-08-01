1db56d1ce5b6c4c2ddeaaa65a1477cd2960b8c98
Ishan-creed <83359975+Ishan-creed@users.noreply.github.com>
2022-08-01 10:46:04 +0530

    Add files via upload

---
+++ routes/posts.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {getPosts, createPosts, updatePost} from '../controllers/posts.js';
+
+
+const router = express.Router();
+
+router.get("/",getPosts);
+router.post("/", createPosts);
+router.patch('/:id', updatePost);
+// router.delete('/:id', deletePost);
+
+export default router;
